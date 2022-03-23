0d2e967161106984b7dcf6e2323af7161ed8ea8d
Srinivas T M <srinivas.t_m@daimler.com>
2022-03-23 16:23:45 +0530

    Added Example 2

---
+++ CallBack/Exp2-Callback.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<functional>
+
+class If1BaseClient
+{
+    public:
+        virtual void connect () = 0;
+
+};
+
+class If1BaseObserver
+{
+    protected:
+        std::function<void()> m_fun;
+
+    public:
+        void onTransportChanged()
+        {
+            m_fun();
+        }
+
+        virtual void setOnConnected(const std::function<void()>& fun) = 0;
+};
+
+class PairingClient : public If1BaseClient, public If1BaseObserver
+{
+    
+    public:
+        void connect() override
+        {
+            for(int i =0 ; i<10000 ; i++)
+                ;
+            onTransportChanged();
+        }
+
+        void setOnConnected(const std::function<void()>& fun) override
+        {
+            m_fun = fun ;
+        }
+};
+
+
+int main()
+{
+    bool connect = false;
+    PairingClient pairingClient;
+    std::cout << "Connect Before = " << connect << std::endl;
+    pairingClient.setOnConnected([&](){
+        connect = true;
+        std::cout << " I am connected. " << std::endl;
+    });
+    std::cout << "Connect Before calling Connect = " << connect << std::endl;
+    pairingClient.connect();
+    std::cout << "Connect After = " << connect << std::endl;
+
+}
