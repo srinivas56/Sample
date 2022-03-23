e0b665213c135c7bb53669e23d9f955647d83c04
srinivas56 <74173137+srinivas56@users.noreply.github.com>
2022-03-23 16:02:13 +0530

    Update main.cpp

--- heap/main.cpp
+++ heap/main.cpp
@@ -1,3 +1,11 @@
+/*
+int *a = new int;
+a is pointing to default-initialized object (which is uninitialized object in this case i.e the value is indeterminate as per the Standard).
+
+int *a = new int();
+a is pointing to value-initialized object (which is zero-initialized object in this case i.e the value is zero as per the Standard).
+*/
+
 #include<iostream>
 using namespace std;
 
